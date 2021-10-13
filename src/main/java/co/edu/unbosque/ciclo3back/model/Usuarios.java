7957e623c783cc7179e716569575d9f6e2705352
idacgo <90365533+idacgo@users.noreply.github.com>
2021-10-13 16:22:20 -0500

    Primera version

--- src/main/java/co/edu/unbosque/ciclo3back/model/Usuarios.java
+++ src/main/java/co/edu/unbosque/ciclo3back/model/Usuarios.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.*;
 
 @Entity
-@Table (name = "usuarios")
+@Table(name = "usuarios")
 public class Usuarios {
 	@Id
 	private long cedula_usuario;
@@ -13,37 +13,46 @@ public class Usuarios {
 	private String email_usuario;
 	private String usuario;
 	private String password;
-	
+
 	@OneToMany(mappedBy = "usuario")
 	private List<Ventas> usuario_venta;
 
 	public long getCedula_usuario() {
 		return cedula_usuario;
 	}
+
 	public void setCedula_usuario(long cedula_usuario) {
 		this.cedula_usuario = cedula_usuario;
 	}
+
 	public String getNombre_usuario() {
 		return nombre_usuario;
 	}
+
 	public void setNombre_usuario(String nombre_usuario) {
 		this.nombre_usuario = nombre_usuario;
 	}
+
 	public String getEmail_usuario() {
 		return email_usuario;
 	}
+
 	public void setEmail_usuario(String email_usuario) {
 		this.email_usuario = email_usuario;
 	}
+
 	public String getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPasword(String password) {
 		this.password = password;
 	}
